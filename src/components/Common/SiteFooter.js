00b6aedd63c779095326650c905da0b383b67174
Bryan W <bhoff1980@gmail.com>
2023-07-11 15:50:02 -0600

    setup login, register and home basic components

--- src/components/Common/SiteFooter.js
+++ src/components/Common/SiteFooter.js
@@ -1,11 +1,16 @@
+import Container from 'react-bootstrap/Container';
 
-
-const SiteFooter = () => {
-  return (
-    <h6>
-      Site Footer 
-    </h6>
-  )
+function SiteFooter() {
+    return (
+        <footer className="py-5 my-5">
+            <Container className="px-4">
+                <p className="text-end">
+                    Icons created by <a href="https://www.flaticon.com/free-icons/people">Flaticon</a>.
+                    <br />&copy; 2022 Cumulus Cycles&nbsp;&nbsp;
+                </p>
+            </Container>
+        </footer>
+    )
 }
 
-export default SiteFooter
+export default SiteFooter;
